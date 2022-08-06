5f35390cb41e7a0187fe2e086baba2dc3531e117
QT <qtests@outlook.com>
2022-08-06 13:00:01 +0200

    First commit

---
+++ get_tardis_futures.py
@@ -0,0 +1,112 @@
+import sys; 
+print('Python %s on %s' % (sys.version, sys.platform))
+
+# Python 3.9.10 (tags/v3.9.10:f2f3f53, Jan 17 2022, 15:14:21) [MSC v.1929 64 bit (AMD64)] on win32
+
+sys.path.extend(['../crypto_trading'])
+
+import os
+import asyncio
+import pandas as pd
+
+import logging
+from pathlib import Path
+
+from market.tardis import Tardis, TardisType, BitMEX, instrument_info, download_book_change_tasks, download_task
+from market.tardis import to_zorro_t6_compatible_csv
+
+from calendar import monthrange, isleap
+import datetime as dt
+
+
+print(f"{dt.date.today()}--------- Starting Download ---------")
+
+# Init
+#
+all_years = [2022]
+all_symbols = [ "BTCUSDT", "ETHUSDT", "ATOMUSDT", "DOTUSDT", "SOLUSDT", "ADAUSDT", 
+                    "EOSUSDT", "BNBUSDT", "XMRUSDT", "DOGEUSDT", "GRTUSDT", "MKRUSDT", "AAVEUSDT"]
+
+
+Tardis.logger().setLevel(logging.INFO)
+cache_dir = Tardis().cache_dir
+zorro_dir = Path(os.path.join(cache_dir)).parent.joinpath("zorro")
+
+if not os.path.exists(zorro_dir):
+    os.makedirs(zorro_dir)
+
+# binance = instrument_info("binance-futures")
+# b_instruments = pd.DataFrame([
+#         instr for instr in binance if instr["datasetId"].endswith("USDT")
+#     ]).set_index("id").sort_index()
+# print (b_instruments)
+
+
+# https://stackoverflow.com/questions/7045920/how-to-calculate-the-number-of-days-in-the-years-between-2-dates-in-python
+def days_in_year(year):
+    return 365 + isleap(year)
+
+
+def days_in_months(year, nof_months=7):
+    total_days = 0
+    for m in range(1, nof_months+1): 
+        _, temp = monthrange(year, m)
+        total_days += temp
+    return (total_days)
+
+
+def months_in_year(year):
+    now = dt.datetime.now()
+    if year == now.year:
+        return (now.month - 1)
+    elif year < now.year:
+        return (12)
+    else:
+        return (None)
+
+
+async def download_year (symbols, dyear = 2021, dmonths = 7, exchange = "binance-futures", dataType = "trade_bar_1m"):
+
+    if dyear is None or dmonths is None:
+        return
+    
+    start_date = f'{dyear}-01-01T00:00:00.000Z'
+
+    if dmonths == 12:
+        num_days = days_in_year(dyear)
+    else:
+        num_days = days_in_months(dyear, dmonths)
+
+    task = download_task(exchange, symbols, dataType, start_date, num_days)
+    # task = [download_task(exchange, s, dataType, start_date, num_days) for s in symbols]
+    await asyncio.gather(task)
+
+
+for year in all_years:
+
+    # Old content
+    content = Tardis().stored_content()
+    df = pd.DataFrame(content)
+
+    # Data download
+    for symb in all_symbols:
+        asyncio.run(download_year(symbols=symb, dyear=year, dmonths=months_in_year(year)))
+    
+    # New content
+    new_content = Tardis().stored_content()
+    new_df = pd.DataFrame(new_content)
+    for idx, arow in new_df[['meta_file', 'symbols']].iterrows():
+        r_meta_file, r_symbols = arow[0], arow[1]
+        if df.empty or (not r_meta_file in df['meta_file'].to_list()):
+            print ( "Converting to Zorro: ", r_symbols[0], " ", r_meta_file )
+            df_tmp = Tardis().load(meta_file=r_meta_file)
+            df_zorro = to_zorro_t6_compatible_csv(df_tmp)
+            df_zorro.to_csv(os.path.join(zorro_dir, f'binance_futures_{r_symbols[0]}_{year}.csv'), index=False)
+        else:
+            print ( "Skipping: ", r_symbols[0], " ", r_meta_file )
+
+# Postprocess
+print ("\nFinito!")
+
+
+
